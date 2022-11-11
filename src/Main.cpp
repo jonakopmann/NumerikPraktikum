a056b9c17b16fd46808cc80b73b6e61464226f91
jona <uftak@student.kit.edu>
2022-11-11 10:38:54 +0100

    Polynom fit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,23 @@
 
 #include <iostream>
-// HI 
-using  namespace std;
+#include <iomanip>
+#include "Process.h"
+
+using namespace std;
+
 int main()
 {
+    // set up cout
+    cout.precision(15);
+    cout.setf(ios::scientific);
+    
+    const int degree = 2;
+    long double* test = new long double[6]
+    {
+        0, 1, 2, 3, 4, 5
+    };
+    Polynom polynom = PolynomialFit(test, 6, degree);
+    polynom.Print();
+
     return 0;
 }
